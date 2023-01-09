39cb30051376e6f5bc0a86dbff128c1419f51285
bodananymanu <fotosmanunan1@gmail.com>
2023-01-08 21:35:47 -0500

    agrega parametro para invitados
    Agrega parametros para invitados

---
+++ js/boda.js
@@ -0,0 +1,11 @@
+// Obtener la URL completa
+const url = new URL(window.location.href);
+
+// Obtener el parámetro de la URL
+const param = url.searchParams.get("invitado");
+
+const divsInvitados = document.getElementsByName("invitado");
+
+for (const miDiv of divsInvitados) {
+    miDiv.innerHTML = "Hola " + param + ", te invitamos a celebrar nuestra boda";
+}
