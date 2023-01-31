92c464022c40af8b099ed2eba3193efe915e8c05
bodananymanu <fotosmanunan1@gmail.com>
2023-01-31 12:37:06 -0500

    Agrega info del evento e información del lugar

--- js/boda.js
+++ js/boda.js
@@ -1,11 +1,41 @@
 // Obtener la URL completa
 const url = new URL(window.location.href);
 
+const divBody = document.getElementById("principal");
+
+
+let listInvitados = [
+    {"id" : 1, "nombre" : "Andrea"  },
+    {"id" : 2, "nombre" : "Alvaro"  },
+    {"id" : 3, "nombre" : "Sofia"  }
+]
+
+
+
 // Obtener el parámetro de la URL
 const param = url.searchParams.get("invitado");
 
+let nombre = null;
+let invitadoEncontrado = false;
+
+
+listInvitados.forEach(function(invitado) {
+    if(invitado.id === parseInt(param)) {
+        nombre = invitado.nombre;
+        invitadoEncontrado = true;
+    }
+});
+
+
 const divsInvitados = document.getElementsByName("invitado");
 
+
+
+if(invitadoEncontrado) {
+    divBody.hidden = false;
+}
+ 
+
 for (const miDiv of divsInvitados) {
-    miDiv.innerHTML = "Hola " + (param ? param : "");
+    miDiv.innerHTML = "Hola " + (nombre ? nombre : "");
 }
